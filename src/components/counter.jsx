2bbcc780bb4e6afeb13f7fe210d9d02e4c5c40fc
BOITUMELO LATAKGOMO <201736639@keyaka.ul.ac.za>
2022-02-06 15:58:27 +0200

    count is incremented

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,26 @@ import React, { Component } from 'react';
  class Counter extends Component {
 
     state = {
-        count: 0,
+        count: 0
        
     };
-    handleIncrement(){
-        console.log("Increment Clicked");
-    }
+    
+    handleIncrement = () => {
+        console.log("test");
+        this.setState({count: this.state.count + 1})
+    };
    
     render() { 
         
+        console.log("test");
         return(
         <div>
              <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
              <button 
              onClick={this.handleIncrement}
-             className="btn btn-secondary btn-sm">Increment</button>
+             className="btn btn-secondary btn-sm">
+                 Increment
+                 </button>
              
         </div>
         );
